2bcd37e8bd2603b18d89fb15d07835c07c2fcdf6
MauriceRedTurtle <89911492+MauriceRedTurtle@users.noreply.github.com>
2021-09-01 16:32:09 +0200

    test vari

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import ReactDOM from 'react-dom';
+
+
+const user = {
+  firstName: 'Maurizio',
+  lastName: 'Querzoli',
+  avatarUrl: 'https://via.placeholder.com/150'
+};
+const image = <img src={user.avatarUrl} alt="avatar"/>;
+const element = <h1 className="test" >Hello, {formatName(user)}!</h1>;
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={user.avatarUrl} className="test" alt="avatar" />
+      
       </header>
-    </div>
+    </div> 
   );
 }
 
+function Tick() {
+  const tickz = (
+    <div>
+      <h1>Hello, world!</h1>
+      <Button>Test bottone con orario .now()  {new Date().toLocaleTimeString()}</Button>
+    </div>
+  );
+
+    ReactDOM.render(tickz, document.getElementById('root'));
+  
+}
+
+setInterval(Tick, 1000);
+
 export default App;
+
+
+
+
+
+//ReactDOM.render([element, image ],document.getElementById('root'));
