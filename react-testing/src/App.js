580494307ea123740c4ca8370c6d45ac749b489a
MauriceRedTurtle <89911492+MauriceRedTurtle@users.noreply.github.com>
2021-09-03 17:16:42 +0200

    test vari

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,50 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from 'react-bootstrap/Button';
-import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
+// import './App.css';
+// import Button from 'react-bootstrap/Button';
+// import ReactDOM from 'react-dom';
 
 
-const user = {
-  firstName: 'Maurizio',
-  lastName: 'Querzoli',
-  avatarUrl: 'https://via.placeholder.com/150'
-};
-const image = <img src={user.avatarUrl} alt="avatar"/>;
-const element = <h1 className="test" >Hello, {formatName(user)}!</h1>;
+// const user = {
+//   firstName: 'Maurizio',
+//   lastName: 'Querzoli',
+//   avatarUrl: 'https://via.placeholder.com/150'
+// };
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// const image = <img src={user.avatarUrl} alt="avatar"/>;
+// const element = <h1 className="test" >Hello, {formatName(user)}!</h1>;
 
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={user.avatarUrl} className="test" alt="avatar" />
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <img src={user.avatarUrl} className="test" alt="avatar" />
       
-      </header>
-    </div> 
-  );
-}
+//       </header>
+//     </div> 
+//   );
+// }
 
-function Tick() {
-  const tickz = (
-    <div>
-      <h1>Hello, world!</h1>
-      <Button>Test bottone con orario .now()  {new Date().toLocaleTimeString()}</Button>
-    </div>
-  );
+// function Tick() {
+//   const tickz = (
+//     <div>
+//       <h1>Test per funzioni</h1>
+//       <Button>Test bottone con orario .now()  {new Date().toLocaleTimeString()}</Button>
 
-    ReactDOM.render(tickz, document.getElementById('root'));
-  
-}
+//       <button className="square" onClick={function() { console.log('click'); }}>
+//        testing 1
+//       </button>
+//       <button className="square" onClick={() => console.log('click') }>
+//        testing 2
+//       </button>
 
-setInterval(Tick, 1000);
+//       {/* <button className="square" onClick={console.log('click')}>
+//        testing 3
+//       </button> */}
+      
+//     </div>
+//   );
+//     ReactDOM.render(tickz, document.getElementById('root'));
+// }
 
-export default App;
+// setInterval(Tick, 1000);
+
+// export default App;
 
 
 
